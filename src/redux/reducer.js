739c5ac17f7848738afd81d2e1c15b7156b19559
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-03-28 19:49:00 +0300

    Add files and made logik

---
+++ src/redux/reducer.js
@@ -0,0 +1,53 @@
+import { statusFilters } from "./constants";
+import { combineReducers } from "redux";
+
+const tasksInitialState = [
+    { id: 0, text: "Learn HTML and CSS", completed: true },
+    { id: 1, text: "Get good at JavaScript", completed: true },
+    { id: 2, text: "Master React", completed: false },
+    { id: 3, text: "Discover Redux", completed: false },
+    { id: 4, text: "Build amazing apps", completed: false },
+  ];
+
+export const tasksReducer = (state = tasksInitialState, action) => {
+  
+  switch (action.type) {
+    case "tasks/addTask": 
+      return [...state, action.payload];
+
+    case "tasks/deleteTask":
+      return state.filter(task => task.id !== action.payload);
+
+    case "tasks/toggleCompleted":
+      return state.map(task => {
+        if (task.id !== action.payload) {
+          return task;
+        }
+        return { ...task, completed: !task.completed };
+      });
+
+      default:
+        return state;
+    }
+  };
+
+const filtersInitialState = {
+    status: statusFilters.all,
+}
+
+export const filtersReducer = (state = filtersInitialState, action) => {
+  switch (action.type) {
+      case "filters/setStatusFilter":
+          return {
+            ...state,
+            status: action.payload,
+          };
+      default:
+          return state;
+  }
+}
+
+export const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  });
